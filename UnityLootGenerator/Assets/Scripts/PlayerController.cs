d609f2bb0960d1ec959b8e809ad5956776407931
mbreaux2023 <mbreaux2023@chadwickschool.org>
2022-03-04 09:11:05 -0800

    3/4 work

--- UnityLootGenerator/Assets/Scripts/PlayerController.cs
+++ UnityLootGenerator/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public SpriteRenderer WeaponRenderer;
     // Start is called before the first frame update
     void Start()
     {
